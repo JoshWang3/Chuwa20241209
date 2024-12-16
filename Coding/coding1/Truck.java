8d2c5612beca220e0aa040ab338865453b3fa9f3
Jiajun Li <1009839598@qq.com>
2024-12-16 14:33:16 -0600

    hw2 commit

---
+++ Coding/coding1/Truck.java
@@ -0,0 +1,9 @@
+package coding1;
+
+public class Truck extends Vehicle {
+    // The constructor of the Truck class calls the constructor of the Vehicle class
+    public Truck(String licensePlate) {
+        super(licensePlate, "Truck");
+    }
+
+}
