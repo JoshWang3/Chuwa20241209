9dc9f4f809b5c315480d1b91561e9d358c2fbc27
yuankuang <kuang.yua@northeastern.edu>
2025-01-16 21:18:58 -0800

    delete wrong files
    deleted files

--- src/main/java/com/chuwa/redbook/dao/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chuwa.redbook.dao;
-
-import com.chuwa.redbook.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
--- src/main/java/com/chuwa/redbook/payload/PostDTO3rd.java
+++
@@ -1,6 +0,0 @@
-package com.chuwa.redbook.payload;
-
-public class PostDTO3rd {
-    private String title;
-    private String description;
-}
