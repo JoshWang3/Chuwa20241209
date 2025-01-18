09731a0cba5258d0284c0c3308137ceaea1135b7
SophieJiang <qtlogresw@gmail.com>
2025-01-17 17:02:49 -0800

    Updated

--- Coding/hw10/LogDemo/LogDemo/src/main/java/com/example/logdemo/LogDemo/LogDemoApplication.java
+++ Coding/hw10/LogDemo/LogDemo/src/main/java/com/example/logdemo/LogDemo/LogDemoApplication.java
@@ -2,12 +2,19 @@ package com.example.logdemo.LogDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.logdemo")
-public class LogDemoApplication {
+public class LogDemoApplication extends SpringBootServletInitializer {
 
+/*
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(YourApplication.class);
+	}
+*/
 	public static void main(String[] args) {
 		SpringApplication.run(LogDemoApplication.class, args);
 	}
