e9c3fc3144bc33a34ba59a45dd2a4fd2ff88c88d
SophieJiang <qtlogresw@gmail.com>
2025-01-17 16:10:24 -0800

    Updated

--- Coding/hw10/LogDemo/LogDemo/src/main/java/com/example/logdemo/controller/DemoController.java
+++ Coding/hw10/LogDemo/LogDemo/src/main/java/com/example/logdemo/controller/DemoController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-// @RequestMapping("/api")
 public class DemoController {
 
     private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
