045cb36e1927d5d874bcbef412d96800170a13d2
Jiajun Li <1009839598@qq.com>
2025-01-26 09:35:48 -0600

    hw13 commit

---
+++ Projects/hw13-project/src/main/java/com/example/hw13project/Hw13ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.hw13project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw13ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw13ProjectApplication.class, args);
+	}
+
+}
---
+++ Projects/hw13-project/src/main/java/com/example/hw13project/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.example.hw13project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/api/test")
+    public String testApi() {
+        return "Test API is working";
+    }
+}
