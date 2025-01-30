a026e44aade2d77cc30f84c606f668c9f74dd4fa
JoshWang3 <163091353+JoshWang3@users.noreply.github.com>
2025-01-30 15:33:23 -0800

    Revert "Zifan_wang/hw14"

--- Coding/hw14/redbook/src/main/java/com/chuwa/redbook/dao/mongo/PostRepositoryMongo.java
+++
@@ -1,7 +0,0 @@
-package com.chuwa.redbook.dao.mongo;
-
-import com.chuwa.redbook.entity.PostMongo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PostRepositoryMongo extends MongoRepository<PostMongo, String> {
-}
